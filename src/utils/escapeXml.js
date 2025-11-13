8f9832e76044bf06fe40776ecef52d13f392f1b4
Fatih Ataç <fatih.atac@meyer.com.tr>
2025-11-13 17:58:10 +0300

    some changes

--- src/utils/escapeXml.js
+++ src/utils/escapeXml.js
@@ -1,12 +1,12 @@
 function escapeXml(unsafe) {
   if (typeof unsafe !== 'string') return unsafe;
-  return unsafe.replace(/[<>&"']/g, function(c) {
+  return unsafe.replace(/[<>&]/g, function(c) {
     switch (c) {
       case '<': return '&lt;';
       case '>': return '&gt;';
       case '&': return '&amp;';
-      case '"': return '&quot;';
-      case "'": return '&apos;';
+      //case '"': return '&quot;';
+      //case "'": return '&apos;';
       default: return c;
     }
   });
