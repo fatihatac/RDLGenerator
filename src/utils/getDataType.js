590762d88c03c8a47a84cf41af99514a2e1ed54d
Fatih Ataç <fatih.atac@meyer.com.tr>
2025-11-14 10:43:59 +0300

    added DateTime detection

--- src/utils/getDataType.js
+++ src/utils/getDataType.js
@@ -1,7 +1,14 @@
 function getRdlTypeName(value) {
-  if (typeof value === 'number') return 'System.Double';
-  if (typeof value === 'boolean') return 'System.Boolean';
-  return 'System.String';
+  if (typeof value === "number") return "System.Double";
+  if (typeof value === "boolean") return "System.Boolean";
+
+  if (typeof value === "string" &&
+    /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}/.test(value)
+  ) {
+    return "System.DateTime";
+  }
+
+  return "System.String";
 }
 
 export { getRdlTypeName };
